225d8d3ec36c787018d7741e5d17752d9b9dfd0f
yogeshcdac <63722232+yogeshcdac@users.noreply.github.com>
2024-03-18 13:21:17 +0530

    C logic building practice

---
+++ c-practice/areaofCircle.c
@@ -0,0 +1,9 @@
+main()
+{
+	int r;
+	float area;
+	printf("enter the radius of circle");
+	scanf("%d",&r);
+	printf("the area of circle is %f",3.14*r*r);
+       return 0;
+}       
---
+++ c-practice/lastdigit.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main()
+{
+	int a;
+	printf("enter a number");
+	scanf("%d",&a);
+	printf("the last digit is %d",a%10);
+	return 0;
+}
+
